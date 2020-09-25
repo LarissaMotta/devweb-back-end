b03457dd6d7c5d970198da9d12a8fb8bfe0fc525
Harã Heique <heikacademicos@hotmail.com>
2020-09-25 19:05:42 -0300

    feat: enable cors

--- beauty-control/src/main.ts
+++ beauty-control/src/main.ts
@@ -5,7 +5,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors();
+    
     const options = new DocumentBuilder()
     .setTitle('Beauty Control API')
     .setDescription('API description')
