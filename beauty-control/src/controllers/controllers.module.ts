c732168836a84b5db1587527d776971567237b70
duraes-antonio <garotoseis@gmail.com>
2021-01-16 05:16:02 -0300

    NEW: Index controller p/ teste heroku

--- beauty-control/src/controllers/controllers.module.ts
+++ beauty-control/src/controllers/controllers.module.ts
@@ -8,6 +8,7 @@ import { SupplierController } from './supplier.controller';
 import { ProductSupplierController } from './product-supplier.controller';
 import { UserSupplierRantigController } from './user-supplier-rating.controller';
 import { ReportController } from './report.controller';
+import {IndexController} from './index.controller';
 
 @Module({
     imports: [
@@ -15,6 +16,7 @@ import { ReportController } from './report.controller';
         AuthStrategiesModule
     ],
     controllers: [
+        IndexController,
         ProductController,
         UserController,
         AuthController,
