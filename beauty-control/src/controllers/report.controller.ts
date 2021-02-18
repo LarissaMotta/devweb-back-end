2ff2d6f17f0ead9d1919317143f4c4b9590e9cb8
Joel Will <joewillb123@gmail.com>
2021-02-17 23:28:57 -0300

    correções de bugs

--- beauty-control/src/controllers/report.controller.ts
+++ beauty-control/src/controllers/report.controller.ts
@@ -3,6 +3,7 @@ import { ApiTags, ApiBearerAuth } from "@nestjs/swagger";
 import { JwtAuthGuard } from "src/auth-strategies/jwt-strategy.guard";
 import { UserRole } from "src/enums/user-role.enum";
 import { Roles } from "src/role/role.decorator";
+import { RolesGuard } from "src/role/role.guard";
 import { ProductService } from "src/services/product.service";
 import { SupplierService } from "src/services/supplier.service";
 
@@ -13,7 +14,7 @@ export class ReportController {
     
     constructor(private productService: ProductService, private supplierService: SupplierService) {}
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get('products')
     @Roles(UserRole.ADMIN)
     async getProductsReport() {
@@ -23,7 +24,7 @@ export class ReportController {
         return { moreUseProducts, stockFlow };
     }
 
-    @UseGuards(JwtAuthGuard)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     @Get('suppliers')
     @Roles(UserRole.ADMIN)
     async getSuppliersReport() {
--- beauty-control/src/entities/user.entity.ts
+++ beauty-control/src/entities/user.entity.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { UserRole } from "src/enums/user-role.enum";
 import { UserSupplierRating } from "./user-supplier-rating.entity";
-import { Length, IsEmail, IsEnum } from "class-validator";
+import { Length, IsEmail, IsEnum, IsOptional } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 import { ProductStockLog } from "./product-stock-log";
 import { ProductSupplier } from "./product-supplier.entity";
@@ -14,24 +14,29 @@ export class User {
 
     @Column('varchar', { length: 40 })
     @Length(4, 40)
+    @IsOptional()
     name: string;
 
     @Column('varchar', { length: 70, unique: true })
     @IsEmail()
     @Length(4, 70)
+    @IsOptional()
     email: string;
 
     @Column('varchar', { length: 100 })
     @Length(6, 40)
+    @IsOptional()
     password: string;
     
     @Column({type: 'enum', enum: UserRole })
     @ApiProperty({ enum: UserRole })
     @IsEnum(UserRole)
+    @IsOptional()
     role: UserRole
 
     @Column()
     @ApiProperty()
+    @IsOptional()
     active: boolean;
 
     @OneToMany(type => UserSupplierRating, usr => usr.user)
